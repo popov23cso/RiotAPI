351e5a86738863541f095d12ed03ef2e18087d35
popov23cso <popov23cso@gmail.com>
2023-02-18 23:02:01 +0200

    Fixed typo

--- RiotAPI/RiotAPI/settings.py
+++ RiotAPI/RiotAPI/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'StatsViewer',
+    'StatViewer',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
