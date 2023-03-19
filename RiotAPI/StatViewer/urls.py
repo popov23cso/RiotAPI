0136f15756b698c56e68620f9154bbff41af3b64
popov23cso <popov23cso@gmail.com>
2023-03-19 16:26:29 +0200

    Added region to url

--- RiotAPI/StatViewer/urls.py
+++ RiotAPI/StatViewer/urls.py
@@ -6,6 +6,6 @@ from . import views
 urlpatterns = [
     path('', views.homepage, name='homepage'),
     path('profile', views.profile, name='profile'),
-    path ('match/<str:match_id>', views.match, name='match')
+    path ('match/<str:region>/<str:match_id>', views.match, name='match')
 
 ]
